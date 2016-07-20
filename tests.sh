c2c57e761a0f6eed5e2d1f01da5790d6ce69edc0
Martin Halamíček <martin@halamicek.cz>
2016-07-20 16:33:24 +0200

    tests docker execution fixes
    [ci skip]

--- tests.sh
+++ tests.sh
@@ -2,5 +2,6 @@
 
 php --version \
   && composer --version \
+  && composer install --prefer-dist --no-interaction \
   && ./vendor/bin/phpcs --standard=psr2 -n --ignore=vendor --extensions=php . \
   && ./vendor/bin/phpunit
