242a4acd1efceac12e4d2f44fea037fcfc10978d
Vladimír Kriška <vlado@keboola.com>
2020-11-20 19:26:18 +0100

    Test empty upload path

--- tests/Keboola/DebugLogUploader/UploaderAbsTest.php
+++ tests/Keboola/DebugLogUploader/UploaderAbsTest.php
@@ -10,9 +10,6 @@ class UploaderAbsTest extends \PHPUnit_Framework_TestCase
     /** @var Filesystem */
     private $fs;
 
-    /** @var UploaderAbs */
-    private $uploader;
-
     /** @var string */
     private $sourcePath = '/tmp/uploader-abs-test-source';
 
@@ -21,15 +18,6 @@ class UploaderAbsTest extends \PHPUnit_Framework_TestCase
         $this->fs = new Filesystem;
         $this->fs->remove($this->sourcePath);
         $this->fs->mkdir($this->sourcePath);
-
-        $this->uploader = new UploaderAbs(
-            $this->getAbsClient(),
-            [
-                'container' => UPLOADER_ABS_CONTAINER,
-                'upload-path' => '/tests/x/',
-                'url-prefix' => '',
-            ]
-        );
 
         parent::setUp();
     }
@@ -57,12 +45,46 @@ ABS uploader upload string
 TXT;
         $name = 'abs-uploader-string.txt';
 
-        $returnUrl = $this->uploader->uploadString($name, $fileContent);
+        $uploader = new UploaderAbs(
+            $this->getAbsClient(),
+            [
+                'container' => UPLOADER_ABS_CONTAINER,
+                'upload-path' => '/tests/x/',
+                'url-prefix' => '',
+            ]
+        );
+
+        $returnUrl = $uploader->uploadString($name, $fileContent);
 
         $client = $this->getAbsClient();
         $blob = $client->getBlob(UPLOADER_ABS_CONTAINER, $returnUrl);
 
         $this->assertEquals('ABS uploader upload string', fread($blob->getContentStream(), 1000));
+        $this->assertEquals('text/plain', $blob->getProperties()->getContentType());
+    }
+
+    public function testUploadStringEmptyUploadPath()
+    {
+        $fileContent = <<<TXT
+ABS uploader upload string empty upload path
+TXT;
+        $name = 'abs-uploader-string-empty-upload-path.txt';
+
+        $uploader = new UploaderAbs(
+            $this->getAbsClient(),
+            [
+                'container' => UPLOADER_ABS_CONTAINER,
+                'upload-path' => '',
+                'url-prefix' => '',
+            ]
+        );
+
+        $returnUrl = $uploader->uploadString($name, $fileContent);
+
+        $client = $this->getAbsClient();
+        $blob = $client->getBlob(UPLOADER_ABS_CONTAINER, $returnUrl);
+
+        $this->assertEquals('ABS uploader upload string empty upload path', fread($blob->getContentStream(), 1000));
         $this->assertEquals('text/plain', $blob->getProperties()->getContentType());
     }
 
@@ -75,7 +97,16 @@ TXT;
         $sourceFile = $this->sourcePath . '/' . $fileName;
         $this->fs->dumpFile($sourceFile, $fileContent);
 
-        $returnUrl = $this->uploader->upload($sourceFile);
+        $uploader = new UploaderAbs(
+            $this->getAbsClient(),
+            [
+                'container' => UPLOADER_ABS_CONTAINER,
+                'upload-path' => '/tests/y/',
+                'url-prefix' => '',
+            ]
+        );
+
+        $returnUrl = $uploader->upload($sourceFile);
 
         $client = $this->getAbsClient();
         $blob = $client->getBlob(UPLOADER_ABS_CONTAINER, $returnUrl);
